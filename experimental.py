9a6675d3d3dcf1db9d47dbfc04f429f2d62c2967
lLorran408 <elorran321@gmail.com>
2025-08-13 12:47:32 -0300

    teste de matematica basica

--- experimental.py
+++ experimental.py
@@ -1,1 +1,8 @@
-print("expeimental")
+print("falor A")
+x = int(input())
+print(" + 10")
+y  = 10
+print("-2")
+z = 2
+soma = x + y - z
+print("o resultado e =", soma)
---
+++ test matemetica.py
@@ -0,0 +1,10 @@
+print("valor A")
+x = int(input())
+print("valor A + 10 - 5 x 5 - 100 + 10=")
+y = 10
+z = 5
+b = 5
+soma = (((x + y) - z)* b)
+soma3 = 100
+soma2 = soma - soma3 + y 
+print("o valor final é=", soma2)
