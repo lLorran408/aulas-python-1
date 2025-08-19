2b95401e7939ecc5176c71283c602e05eeef20a4
lLorran408 <elorran321@gmail.com>
2025-08-19 14:29:18 -0300

    Add files via upload

---
+++ rr.py
@@ -0,0 +1,8 @@
+numero = int(input())
+
+if numero > 0:
+    print("Número positivo")
+elif numero < 0:
+    print("Número negativo")
+else:
+    print("Número é zero")
---
+++ test +-=.py
@@ -0,0 +1,10 @@
+# teste plse/if
+print("digite sua idade")
+idade = int(input())
+if idade >= 18:
+    print("você pode assesar")
+elif idade < 18:
+    print("você não pode assesar!") 
+if idade >= 55:
+    print("o suporte") 
+    
