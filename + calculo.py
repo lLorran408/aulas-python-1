28a5028a83abb7fcf0d51daf3739139362f49640
LLorran408 <elorran321@gmail.com>
2025-08-25 12:41:12 -0300

    mas treino

---
+++ + calculo.py
@@ -0,0 +1,6 @@
+# calculo do imposto
+valor = int(input("digite o valor"))
+Qparcelas = int(input("qual a quantidade de parcalas?"))
+parcelas = valor / Qparcelas
+
+print("valor a pagar", parcelas)
---
+++ 2.py
@@ -0,0 +1,6 @@
+# calculo ao cubo
+
+numero = int(input("valor 1"))
+quadrado = int(input("ao cubo de"))
+valorfinal = numero * quadrado
+print("resultado",valorfinal)
